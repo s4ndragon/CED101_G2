6f54d804d9b02b3496c96ea535ab2745dcb8e59a
chloe9855 <liu870707@gmail.com>
2021-01-05 21:59:17 +0800

    連後端

---
+++ src/php/vote.php
@@ -0,0 +1,15 @@
+<?php 
+ 
+try {
+	require_once("./connectBooks.php");
+	$sql = "select * from garden";
+	$products = $pdo->prepare($sql);
+	$products->execute();
+	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($prodRows); //把陣列編碼成json字串傳到前端 echo印出json字串
+} catch (PDOException $e) {
+	echo "錯誤原因 : ", $e->getMessage(), "<br>";
+	echo "錯誤行號 : ", $e->getLine(), "<br>";
+	// echo "系統錯誤, 請通知系統維護人員<br>";
+}
+?>
