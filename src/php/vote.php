a6794f0616416f7fffecc077cb49213b055cc697
chloe9855 <liu870707@gmail.com>
2021-01-06 21:09:37 +0800

    new

--- src/php/vote.php
+++ src/php/vote.php
@@ -2,11 +2,18 @@
  
 try {
 	require_once("./connectBooks.php");
-	$sql = "select * , GARD_VOTE/GARD_CLICK 'GARD_AVG' from garden";
-	$products = $pdo->prepare($sql);
-	$products->execute();
-	$prodRows = $products->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($prodRows); //把陣列編碼成json字串傳到前端 echo印出json字串
+	$sql = "select * , GARD_VOTE/GARD_CLICK 'GARD_AVG' from garden order by GARD_AVG desc";
+	$garden = $pdo->prepare($sql);
+	$garden->execute();
+	$gardenRows = $garden->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($gardenRows); //把陣列編碼成json字串傳到前端 echo印出json字串
+
+	// $sql = "select * , GARD_VOTE/GARD_CLICK 'GARD_AVG' from garden order by GARD_AVG desc";
+	// $garden = $pdo->prepare($sql);
+	// $garden->execute();
+	// $gardenRowsEnd = $garden->fetchAll(PDO::FETCH_ASSOC);
+	// echo json_encode($gardenRowsEnd);
+
 } catch (PDOException $e) {
 	echo "錯誤原因 : ", $e->getMessage(), "<br>";
 	echo "錯誤行號 : ", $e->getLine(), "<br>";
