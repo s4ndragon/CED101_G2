445c99885878e6e1427e4cc04cc80fb43e032608
postintw <postintw@gmail.com>
2021-01-09 08:57:11 +0800

    調整計分版型

--- src/js/function.js
+++ src/js/function.js
@@ -4,3 +4,18 @@ function maxlength(obj, lg = 10) {
 }
 }
 maxlength('input');
+// 長度限制
+// 全選帶有change事件
+function chkAll(obj){
+    var ischecked = obj.checked;
+    $("input:checkbox[child]").each(function () {
+        $(this).prop('checked',ischecked).change();
+    });
+};
+// 表格內全選，DOM路徑table>tr>td>input:checkbox，移動到table>tr>td:first>input:checkbox
+// <input type="checkbox" onclick="checkAll(this)">
+function checkAll(obj) {
+    $(obj).parent.parent.find('tr').each(function () {
+        $(this).first.first.checked = obj.checked;
+    });
+}
