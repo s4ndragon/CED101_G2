b930819b4cdcc22fd485ab4847d9eaa6e8ee7c09
ytt751202 <ytt751202@gmail.com>
2021-01-12 21:29:24 +0800

    1. 首頁rwd間距調整；2.首頁商品文宣更新；3.新增加入收藏功能；新增後台index、scss、js及PHP

---
+++ src/js/admin_member_vue.js
@@ -0,0 +1,63 @@
+new Vue({
+	el: '#app',
+	data: {
+        mem_id: '',
+        members:'',
+		
+	},
+	
+	
+	
+    methods: {//函數放這裡
+        
+        get_mems:async function () {
+				const res = await fetch('./phps/admin_GetMembers.php', {
+				method: 'POST',
+				mode: 'same-origin',
+				credentials: 'same-origin',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				// body: JSON.stringify({
+				// 	add_no: this.add_no,
+				// 	add_id: this.add_id,
+				// 	add_name: this.add_name,
+				// 	add_psw: this.add_psw,
+
+				// }),
+			
+			}).then(function(data){
+				return data.json()
+			})
+			//完成後 重新撈取一次資料 把res回傳到members裡面
+			this.members = res
+		},
+
+		query_mem_id: async function (mem_id) {
+			console.log(mem_id)
+
+			const res = await fetch('./phps/admin_QueMembers.php', {
+				method: 'POST',
+				mode: 'same-origin',
+				credentials: 'same-origin',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					mem_id: this.mem_id,
+				}),
+			}).then(function(data){
+				return data.json()
+			})
+          //重新撈取一次細項列表
+			this.get_mems(this.mem_id)
+		},
+	},
+
+
+	created(){
+		this.get_mems()
+	},
+})
+
+
