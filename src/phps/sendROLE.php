a201fe4ac9b9f353d883f8534615be80707dd91b
postintw <postintw@gmail.com>
2021-01-17 21:44:03 +0800

    角色

--- src/phps/sendROLE.php
+++ src/phps/sendROLE.php
@@ -3,28 +3,30 @@
 <?php 
  try {
     require_once("./connect_game.php");
-if(isset($_GET['update'])){
-    $game=$_GET["NO[]"];
+if(isset($_POST['update'])){
+    $game=$_POST["NO[]"];
     foreach($game as $i => $data){
-	$sql = "update ROLE SET IMG= :IMG,DATA_HPS = :hps,DATA_HP = :hp,DATA_SPEED = :speed,DATA_DAMAGE = :damage,DATA_DEFENSE = :defense from ROLE   where ROLE_NO = :ROLE_NO ";
+	$sql = "update `role` SET IMG= :IMG,DATA_HPS = :hps,DATA_HP = :hp,DATA_SPEED = :speed,DATA_DAMAGE = :damage,DATA_DEFENSE = :defense from ROLE   where ROLE_NO = :ROLE_NO ";
     $gamerole = $pdo->prepare($sql);
-    $gamerole->bindValue(":IMG", $_GET["pic[]"][$i]);
-    $gamerole->bindValue(":star", $_GET["star[]"][$i]);
-    $gamerole->bindValue(":hp", $_GET["hp[]"][$i]);
-    $gamerole->bindValue(":hps", $_GET["hps[]"][$i]);
-    $gamerole->bindValue(":speed", $_GET["speed[]"][$i]);
-	$gamerole->bindValue(":damage", $_GET["damage[]"][$i]);
-	$gamerole->bindValue(":defense", $_GET["defense[]"][$i]);
+    $gamerole->bindValue(":IMG", $_POST["pic[]"][$i]);
+    $gamerole->bindValue(":star", $_POST["star[]"][$i]);
+    $gamerole->bindValue(":hp", $_POST["hp[]"][$i]);
+    $gamerole->bindValue(":hps", $_POST["hps[]"][$i]);
+    $gamerole->bindValue(":speed", $_POST["speed[]"][$i]);
+	$gamerole->bindValue(":damage", $_POST["damage[]"][$i]);
+	$gamerole->bindValue(":defense", $_POST["defense[]"][$i]);
 	$gamerole->execute();
     }
+    echo "操作完成";
 }else{
-    $game=$_GET["NO[]"];
+    $game=$_POST["NO[]"];
     foreach($game as $i => $data){
-	$sql = "delete from `ROLE` where `ROLE_NO` = :ROLE_NO";
+	$sql = "delete from `role` where `ROLE_NO` = :ROLE_NO";
     $gamerole = $pdo->prepare($sql);
-    $gamerole->bindValue(":ROLE_NO", $_GET["NO[]"][$i]);
+    $gamerole->bindValue(":ROLE_NO", $_POST["NO[]"][$i]);
 	$gamerole->execute();
     }
+    echo "刪除成功";
 }
 } catch (PDOException $e) {
 	$pdo->rollBack();
