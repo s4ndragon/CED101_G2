3450a16c997f7b81fa9add10a82ea7e31a1ac304
as553uua123 <as553uua123@yahoo.com.tw>
2021-01-18 18:08:01 +0800

    分享預覽更換

--- src/phps/getArticles.php
+++ src/phps/getArticles.php
@@ -11,9 +11,21 @@ try {
 	$articles = $pdo->prepare($sql);
 	$articles->execute();
 	$articlesRows = $articles->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($articlesRows);
+	// echo json_encode($articlesRows);
+	 
 
-	$sql = "updates ";
+
+
+	$sql = "select ART_NO from art_favorite where MEM_NO=2";
+	$art_favorite = $pdo->prepare($sql);
+	// $sql = "select ART_NO from art_favorite where MEM_NO=:MEM_NO";
+	// $art_favorite->bindValue(":MEM_NO", $_SESSION["MEM_NO"]);
+	$art_favorite->execute();
+	$art_favoriteRows = $art_favorite->fetchAll();
+	// echo json_encode($art_favoriteRows);
+	$result = [$articlesRows, $art_favoriteRows];
+	echo json_encode($result);
+
 } catch (PDOException $e) {
 	echo "錯誤原因 : ", $e->getMessage(), "<br>";
 	echo "錯誤行號 : ", $e->getLine(), "<br>";
