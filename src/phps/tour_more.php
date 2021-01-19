403701c51a0874d1449307f5491646b8196cce27
chloe9855 <liu870707@gmail.com>
2021-01-19 19:28:09 +0800

    未登入會員畫面渲染判斷

--- src/phps/tour_more.php
+++ src/phps/tour_more.php
@@ -11,12 +11,17 @@ try {
 	$tourRows = $tour->fetchAll(PDO::FETCH_ASSOC);
 	
 	//找這個會員有參加的所有揪團
-	$sql = "select TOUR_ID from tour_join where MEM_NO = :MEM_NO";
-	$tour2 = $pdo->prepare($sql);
-	$MEM_NO = $_SESSION["MEM_NO"];
-  	$tour2->bindValue(":MEM_NO", $MEM_NO);
-	$tour2->execute();
-	$joinRows = $tour2->fetchAll(PDO::FETCH_ASSOC);
+	if(isset($_SESSION["MEM_NO"])){
+		$sql = "select TOUR_ID from tour_join where MEM_NO = :MEM_NO";
+		$tour2 = $pdo->prepare($sql);
+		$MEM_NO = $_SESSION["MEM_NO"];
+		$tour2->bindValue(":MEM_NO", $MEM_NO);
+		$tour2->execute();
+		$joinRows = $tour2->fetchAll(PDO::FETCH_ASSOC);
+	}else{
+		$joinRows = "{}";
+	}
+	
 	
 	$sql = "select * from tour join hotel on tour.HOTEL_ID = hotel.HOTEL_ID where TOUR_ID = :TOUR_ID";
 	$hotel = $pdo->prepare($sql);
@@ -32,28 +37,37 @@ try {
 
 	$sql = "select * from  tour_msg join member on tour_msg.MEM_NO = member.MEM_NO where tour_msg.TOUR_ID = :TOUR_ID";
 	$msg = $pdo->prepare($sql);
-	$MEM_NO = $_SESSION["MEM_NO"];
 	$msg->bindValue(":TOUR_ID", $_POST["TOUR_ID"]);
 	$msg->execute();
 	$msgRows = $msg->fetchAll(PDO::FETCH_ASSOC);
 
 	//找出這個會員收藏的所有揪團
-	$sql = "select TOUR_ID from tour_collect where MEM_NO = :MEM_NO";
-	$tour3 = $pdo->prepare($sql);
-	$MEM_NO = $_SESSION["MEM_NO"];
-  	$tour3->bindValue(":MEM_NO", $MEM_NO);
-	$tour3->execute();
-	$loveRows = $tour3->fetchAll(PDO::FETCH_ASSOC);
+	if(isset($_SESSION["MEM_NO"])){
+		$sql = "select TOUR_ID from tour_collect where MEM_NO = :MEM_NO";
+		$tour3 = $pdo->prepare($sql);
+		$MEM_NO = $_SESSION["MEM_NO"];
+		$tour3->bindValue(":MEM_NO", $MEM_NO);
+		$tour3->execute();
+		$loveRows = $tour3->fetchAll(PDO::FETCH_ASSOC);
+	}else{
+		$loveRows = "{}";
+	}
+	
 
 	//找此會員是否收藏過此揪團 
 	//count(*)為回傳資料筆數 (不是1就是0，1表示有這筆資料即有收藏過，0表示有這筆資料即沒收藏過)
-	$sql = "select count(*) 'like_before' from tour_collect where MEM_NO = :MEM_NO and TOUR_ID = :TOUR_ID";
-	$tour4 = $pdo->prepare($sql);
-	$tour4->bindValue(":TOUR_ID", $_POST["TOUR_ID"]);
-	$MEM_NO = $_SESSION["MEM_NO"];
-	$tour4->bindValue(":MEM_NO", $MEM_NO);
-	$tour4->execute();
-	$savedLikes = $tour4->fetchAll(PDO::FETCH_ASSOC);
+	if(isset($_SESSION["MEM_NO"])){
+		$sql = "select count(*) 'like_before' from tour_collect where MEM_NO = :MEM_NO and TOUR_ID = :TOUR_ID";
+		$tour4 = $pdo->prepare($sql);
+		$tour4->bindValue(":TOUR_ID", $_POST["TOUR_ID"]);
+		$MEM_NO = $_SESSION["MEM_NO"];
+		$tour4->bindValue(":MEM_NO", $MEM_NO);
+		$tour4->execute();
+		$savedLikes = $tour4->fetchAll(PDO::FETCH_ASSOC);
+	}else{
+		$savedLikes = "{}";
+	}
+	
 
 
 	$dataRows=[$tourRows,$joinRows,$hotelRows,$foodRows,$msgRows,$loveRows,$savedLikes];
--- src/phps/vote_more.php
+++ src/phps/vote_more.php
@@ -10,13 +10,17 @@ try {
 	$garden->execute();
 	$gardenRows = $garden->fetchAll(PDO::FETCH_ASSOC);
 	
-
-	$sql = "select VOTE_DATE from member where MEM_NO = :MEM_NO";
-	$date = $pdo->prepare($sql);
-	$MEM_NO = $_SESSION["MEM_NO"];
-	$date->bindValue(":MEM_NO", $MEM_NO);
-	$date->execute();
-	$pastDate = $date->fetchAll(PDO::FETCH_ASSOC);
+	if(isset($_SESSION["MEM_NO"])){
+		$sql = "select VOTE_DATE from member where MEM_NO = :MEM_NO";
+		$date = $pdo->prepare($sql);
+		$MEM_NO = $_SESSION["MEM_NO"];
+		$date->bindValue(":MEM_NO", $MEM_NO);
+		$date->execute();
+		$pastDate = $date->fetchAll(PDO::FETCH_ASSOC);
+	}else{
+		$pastDate = "{}";
+	}
+	
 
 	$sql = "select * from tour join garden on tour.GARD_ID = garden.GARD_ID where garden.GARD_ID = :GARD_ID or garden.GARD_ID = 6 limit 3";
 	$tour = $pdo->prepare($sql);
@@ -25,20 +29,30 @@ try {
 	$tourRows = $tour->fetchAll(PDO::FETCH_ASSOC);
 
 	//抓這個茶園的 留言 | 留言會員資料
-	$sql = "select member.MEM_NICNAME, member.MEM_IMG, garden_msg.MSG_DATE, garden_msg.MSG_CONTENT, garden_msg.MSG_NO from
+	if(isset($_SESSION["MEM_NO"])){
+		$sql = "select member.MEM_NICNAME, member.MEM_IMG, garden_msg.MSG_DATE, garden_msg.MSG_CONTENT, garden_msg.MSG_NO from
 				 member join garden_msg on member.MEM_NO = garden_msg.MEM_NO where garden_msg.GARD_ID = :GARD_ID";
-	$mem = $pdo->prepare($sql);
-	$MEM_NO = $_SESSION["MEM_NO"];
-	$mem->bindParam(":GARD_ID", $_POST["GARD_ID"]);
-	$mem->execute();
-	$msgRows = $mem->fetchAll(PDO::FETCH_ASSOC);
+		$mem = $pdo->prepare($sql);
+		$MEM_NO = $_SESSION["MEM_NO"];
+		$mem->bindParam(":GARD_ID", $_POST["GARD_ID"]);
+		$mem->execute();
+		$msgRows = $mem->fetchAll(PDO::FETCH_ASSOC);
+	}else{
+		$msgRows = "{}";
+	}
+	
 
-	$sql = "select TOUR_ID from tour_collect where MEM_NO = :MEM_NO";
-	$like = $pdo->prepare($sql);
-	$MEM_NO = $_SESSION["MEM_NO"];
-	$like->bindValue(":MEM_NO", $MEM_NO);
-	$like->execute();
-	$likeRows = $like->fetchAll(PDO::FETCH_ASSOC);
+	if(isset($_SESSION["MEM_NO"])){
+		$sql = "select TOUR_ID from tour_collect where MEM_NO = :MEM_NO";
+		$like = $pdo->prepare($sql);
+		$MEM_NO = $_SESSION["MEM_NO"];
+		$like->bindValue(":MEM_NO", $MEM_NO);
+		$like->execute();
+		$likeRows = $like->fetchAll(PDO::FETCH_ASSOC);
+	}else{
+		$likeRows = "{}";
+	}
+	
 	
 
 	$dataRows = [$gardenRows,$pastDate,$tourRows,$msgRows,$likeRows];
