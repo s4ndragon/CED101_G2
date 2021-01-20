1a7a9343e4c50edb757b7191b8c7eaab5efef395
chloe9855 <liu870707@gmail.com>
2021-01-20 21:41:56 +0800

    收藏功能完成

--- src/phps/vote_more.php
+++ src/phps/vote_more.php
@@ -29,13 +29,12 @@ try {
 	$tourRows = $tour->fetchAll(PDO::FETCH_ASSOC);
 
 	//抓這個茶園的 留言 | 留言會員資料
-
-		$sql = "select member.MEM_NICNAME, member.MEM_IMG, garden_msg.MSG_DATE, garden_msg.MSG_CONTENT, garden_msg.MSG_NO from
-				 member join garden_msg on member.MEM_NO = garden_msg.MEM_NO where garden_msg.GARD_ID = :GARD_ID";
-		$mem = $pdo->prepare($sql);
-		$mem->bindParam(":GARD_ID", $_POST["GARD_ID"]);
-		$mem->execute();
-		$msgRows = $mem->fetchAll(PDO::FETCH_ASSOC);
+	$sql = "select member.MEM_NICNAME, member.MEM_IMG, garden_msg.MSG_DATE, garden_msg.MSG_CONTENT, garden_msg.MSG_NO from
+			 member join garden_msg on member.MEM_NO = garden_msg.MEM_NO where garden_msg.GARD_ID = :GARD_ID";
+	$mem = $pdo->prepare($sql);
+	$mem->bindParam(":GARD_ID", $_POST["GARD_ID"]);
+	$mem->execute();
+	$msgRows = $mem->fetchAll(PDO::FETCH_ASSOC);
 
 	
 
@@ -45,12 +44,28 @@ try {
 		$MEM_NO = $_SESSION["MEM_NO"];
 		$like->bindValue(":MEM_NO", $MEM_NO);
 		$like->execute();
-		$likeRows = $like->fetchAll(PDO::FETCH_ASSOC);
+		$likeRows_1 = $like->fetchAll(PDO::FETCH_ASSOC);
+		$likeRows = [];
+		for($k=0;$k < count($likeRows_1);$k++){
+			array_push($likeRows,$likeRows_1[$k]["TOUR_ID"]);
+		};
 	}else{
 		$likeRows = "{}";
 	}
 	
+
+
+	for($k=0;$k<count($tourRows);$k++){
+
+		if(in_array($tourRows[$k]["TOUR_ID"],$likeRows)){
+			$tourRows[$k]["img"] = "./images/common/like.png";
+		}else{
+			$tourRows[$k]["img"] = "images/common/heart.png";
+		}
+	}
 	
+
+
 
 	$dataRows = [$gardenRows,$pastDate,$tourRows,$msgRows,$likeRows];
 	echo json_encode($dataRows);  //把陣列編碼成json字串傳到前端 echo印出json字串
