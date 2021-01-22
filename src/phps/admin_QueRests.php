ea47471665f85b73edfc9f01d19667d9ed96ab1c
ytt751202 <ytt751202@gmail.com>
2021-01-22 18:01:58 +0800

    1. 後台php查詢指令修改；2.CSS更新

--- src/phps/admin_QueRests.php
+++ src/phps/admin_QueRests.php
@@ -12,9 +12,9 @@ try {
   
     //接到之後要做的SQL指令
     //:後面+名字會變成一個變數 ->Php 寫sql的時候的寫法
-	$sql = "select * 
-            from restaurant
-            where RESTAURANT_ID= :RESTAURANT_ID
+	$sql = "select a.RESTAURANT_ID RESTAURANT_ID, a.RESTAURANT_NAME RESTAURANT_NAME, a.RESTAURANT_INFRO RESTAURANT_INFRO, a.RESTAURANT_STATUS RESTAURANT_STATUS, b.GARD_ID GARD_ID
+            from restaurant a join restaurant_list b on a.RESTAURANT_ID=b.RESTAURANT_ID
+            where a.RESTAURANT_ID= :RESTAURANT_ID
             ";
 
     // $grouporddata = $pdo->query($sql);
